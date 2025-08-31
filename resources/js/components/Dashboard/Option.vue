add95976e01ac4b2befd06e252b53ecd05e88aeb
Lucas Paz <eu@lucaspaz.com>
2025-08-31 07:25:40 -0300

    feat: add Option component for dashboard with title and image props

---
+++ resources/js/components/Dashboard/Option.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(['title', 'image']);
+</script>
+
+<template>
+    <div class="flex cursor-pointer flex-col rounded-xl bg-primary p-10 text-white lg:px-[54px] lg:py-8">
+        <img :src="props.image" width="263" height="175" :alt="props.title" class="mx-auto" draggable="false" />
+
+        <div class="mt-auto pt-4 text-center font-bold">{{ props.title }}</div>
+    </div>
+</template>
