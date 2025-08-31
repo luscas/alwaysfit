68eda879f40fb1b02a48cc7525d69809d3a8fa4b
Lucas Paz <eu@lucaspaz.com>
2025-08-31 05:49:38 -0300

    refactor: remove unused authentication routes and update dashboard route name

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
-})->name('home');
+})->name('dashboard');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
