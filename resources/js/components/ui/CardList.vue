05545fcc231c8bc53fc0c6049afa62b061aad7c7
Lucas Paz <eu@lucaspaz.com>
2025-08-31 08:38:59 -0300

    feat: create CardList component for reusable card layout

---
+++ resources/js/components/ui/CardList.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(["class"]);
+</script>
+
+<template>
+    <div
+        :class="[
+            'bg-white border border-mist w-full shadow-[0_16px_40px_#1D1D2608] rounded-lg text-graphite',
+            props.class,
+        ]"
+    >
+        <slot />
+    </div>
+</template>
