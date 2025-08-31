badcd5440b6556408dda96b5a82adee395c77ca9
Lucas Paz <eu@lucaspaz.com>
2025-08-31 07:05:00 -0300

    feat: update dashboard route to render Dashboard component instead of Welcome

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Dashboard');
 })->name('dashboard');
 
 require __DIR__.'/auth.php';
