c3d13176cb14e24d667099a368f639f89868273e
Lucas Paz <eu@lucaspaz.com>
2025-08-31 06:29:54 -0300

    feat: create AuthLayout component with Header and Footer

---
+++ resources/js/layouts/AuthLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Footer from '@/partials/Footer.vue';
+import Header from '@/partials/Header.vue';
+</script>
+
+<template>
+    <Header />
+    <slot />
+    <Footer />
+</template>
