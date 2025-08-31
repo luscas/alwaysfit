15cc61642a868e44048988019e3d2ee7337b0fb8
Lucas Paz <eu@lucaspaz.com>
2025-08-31 05:50:10 -0300

    feat: create User component with welcome message and page props display

---
+++ resources/js/pages/User.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div>
+        <h1>Voce está logado!</h1>
+        <pre>{{ JSON.stringify($page.props) }}</pre>
+    </div>
+</template>
