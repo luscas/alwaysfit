170574839ef0869803eb2a4783329db43b636e31
Lucas Paz <eu@lucaspaz.com>
2025-08-31 07:26:22 -0300

    refactor: standardize import statements and improve template structure in Dashboard.vue

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,46 +1,48 @@
 <script setup>
 // ** Inertia
-import { Link } from "@inertiajs/vue3";
+import { Head, Link } from '@inertiajs/vue3';
 
 // ** Layouts
-import UserLayout from "@/layouts/UserLayout.vue";
+import UserLayout from '@/layouts/UserLayout.vue';
 
 // ** Components
-import DashboardOption from "@/components/Dashboard/Option.vue";
+import DashboardOption from '@/components/Dashboard/Option.vue';
 
 // ** Assets
-import imageTraining from "~/assets/uploads/1.png";
-import imageFitness from "~/assets/uploads/2.png";
-import imageProgress from "~/assets/uploads/3.png";
+import imageTraining from '~/assets/uploads/1.png';
+import imageFitness from '~/assets/uploads/2.png';
+import imageProgress from '~/assets/uploads/3.png';
 
 const dashboardOptions = [
     {
-        url: "/trainings",
-        title: "Treinos",
+        url: '/trainings',
+        title: 'Treinos',
         image: imageTraining,
     },
     {
-        url: "/nutrition-plans",
-        title: "Plano nutricional",
+        url: '/nutrition-plans',
+        title: 'Plano nutricional',
         image: imageFitness,
     },
     {
-        url: "/progress",
-        title: "Progresso",
+        url: '/progress',
+        title: 'Progresso',
         image: imageProgress,
     },
 ];
 </script>
 
 <template>
+    <Head title="Dashboard" />
+
     <UserLayout title="Dashboard">
-        <main class="container px-6 py-10 xl:px-[135px] xl:py-[72px] mx-auto">
+        <main class="container mx-auto px-6 py-10 xl:px-[135px] xl:py-[72px]">
             <div class="text-[#4D4B5C]">
                 Olá <b>{{ $page.props.auth.user.name }}</b
                 >! Seja bem-vindo!
             </div>
 
-            <div class="grid grid-cols-12 mt-8 gap-9">
+            <div class="mt-8 grid grid-cols-12 gap-9">
                 <Link
                     :as="DashboardOption"
                     :href="option.url"
