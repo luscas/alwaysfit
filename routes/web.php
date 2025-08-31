4b1062d33068b30719e75b189f8c6fdfae64ea20
Lucas Paz <eu@lucaspaz.com>
2025-08-31 08:39:16 -0300

    feat: organize TrainingController routes under a dedicated prefix

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\{DashboardController, TrainingController};
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::group(['prefix' => '/trainings'], function () {
+        Route::get('/', [TrainingController::class, 'index'])->name('trainings');
+        Route::get('/{training}', [TrainingController::class, 'show'])->name('trainings.detail');
+        Route::post('/{training}/log', [TrainingController::class, 'log'])->name('trainings.log');
+    });
 });
 
 
