ec26a93ad8df0d0b09dcfd3654655660e097dad4
Lucas Paz <eu@lucaspaz.com>
2025-08-31 10:32:51 -0300

    fix: update Vite reference from app.js to app.ts in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
 
-    @vite('resources/js/app.js')
+    @vite('resources/js/app.ts')
     @inertiaHead
 </head>
 
