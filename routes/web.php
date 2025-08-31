75b99a8dd712b57d0f604b17d4af2c2537f88dba
Lucas Paz <eu@lucaspaz.com>
2025-08-31 08:51:55 -0300

    feat: add nutritionPlans relationship to User model and update routes to include NutritionPlanController

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-use App\Http\Controllers\{DashboardController, TrainingController};
+use App\Http\Controllers\{DashboardController, TrainingController, NutritionPlanController};
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
