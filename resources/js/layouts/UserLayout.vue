3162fe0c720e4690e7f90108fd529ec1d1359dc7
Lucas Paz <eu@lucaspaz.com>
2025-08-31 07:03:38 -0300

    feat: create UserLayout and Dashboard components with layout structure and user data display

---
+++ resources/js/layouts/UserLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+// ** Partials
+import Footer from '@/partials/Footer.vue';
+import Header from '@/partials/Header.vue';
+import Navbar from '@/partials/Navbar.vue';
+
+const props = defineProps(['title', 'redirectBack']);
+</script>
+
+<template>
+    <Header />
+
+    <Navbar :title="props.title" :redirectBack="props.redirectBack" />
+
+    <slot />
+
+    <Footer />
+</template>
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import UserLayout from '@/layouts/UserLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const user: User = (page.props.auth as any).user as User;
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <UserLayout>
+        <h1>Olá, {{ user.name }}</h1>
+        <pre>{{ JSON.stringify($page.props, null, 2) }}</pre>
+    </UserLayout>
+</template>
