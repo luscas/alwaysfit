273814a1ff288d94179e4acf94418ada4b686374
Lucas Paz <eu@lucaspaz.com>
2025-08-31 08:38:52 -0300

    feat: add Progress component to display training progress visually

---
+++ resources/js/components/Training/Progress.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['done', 'remaining', 'target']);
+</script>
+
+<template>
+    <div class="flex h-6 w-full overflow-hidden rounded-lg">
+        <div class="flex items-center justify-center bg-success text-xs text-white" :style="{ width: `${(done / target) * 100}%` }">
+            {{ done }} dias
+        </div>
+
+        <div class="flex items-center justify-center bg-error text-xs text-white" :style="{ width: `${(remaining / target) * 100}%` }">
+            {{ remaining }} dias
+        </div>
+    </div>
+</template>
