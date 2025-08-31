5a45a06a070a3bbc5ea8baea888bc2d2d4d78563
Lucas Paz <eu@lucaspaz.com>
2025-08-31 06:29:48 -0300

    feat: add Google Fonts preconnect and Montserrat font stylesheet

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,11 @@
     <link rel="icon" href="/favicon.ico" sizes="any">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
